238500457b98660255c621512d6425868f0efa2d
ejfs1g10 <ejfs1g10@ecs.soton.ac.uk>
2012-02-20 23:31:50 +0000

    *Added InnerFrame

---
+++ src/viewTest2/viewTest2/src/InnerFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.JInternalFrame;
+
+public class InnerFrame extends JInternalFrame{
+
+	public InnerFrame(){
+		setMaximizable(true);
+		setClosable(true);
+		setResizable(true);
+	}
+
+}
