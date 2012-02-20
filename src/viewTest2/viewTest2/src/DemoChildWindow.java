e8139a850f054d8eaeaf67bfe19b8d0b809c223c
ejfs1g10 <ejfs1g10@ecs.soton.ac.uk>
2012-02-20 17:39:12 +0000

    *Discovered the possibility to use native look and feel on Inner Frames

--- src/viewTest2/viewTest2/src/DemoChildWindow.java
+++ src/viewTest2/viewTest2/src/DemoChildWindow.java
@@ -1,10 +1,14 @@
 import javax.swing.JInternalFrame;
+import javax.swing.UIManager;
 
 public class DemoChildWindow extends JInternalFrame{
-
+	
 	public DemoChildWindow(){
-		super();
+		super("Child");
 		setSize(200,200);
+		setClosable(true);
+		setMaximizable(true);
+		setResizable(true);
 		setVisible(true);
 	}
 	
--- src/viewTest2/viewTest2/src/MainFrame.java
+++ src/viewTest2/viewTest2/src/MainFrame.java
@@ -1,11 +1,23 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 public class MainFrame extends JFrame{
 
+	  private static void setNativeLookAndFeel() {
+		    try {
+		      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		    } catch(Exception e) {
+		      System.out.println("Error setting native LAF: " + e);
+		    }
+		  }
+	
 	public static void main(String[] args){
 		
 		MainFrame mf = new MainFrame();
 		mf.setContentPane(new DesktopPane());
+		mf.getContentPane().setBackground(Color.LIGHT_GRAY);
 		mf.getContentPane().add(new DemoChildWindow());
 		mf.getContentPane().add(new DemoChildWindow());
 		
@@ -15,6 +27,7 @@ public class MainFrame extends JFrame{
 	public MainFrame(){
 		super("Main Frame");
 		setSize(400, 400);
+		setNativeLookAndFeel();
 		setVisible(true);
 	}
 	
