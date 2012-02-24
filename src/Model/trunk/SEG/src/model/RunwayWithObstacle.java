23cdab6a6320ac2581a3bf3cea829f4d5f19bda4
ycc1g11 <ycc1g11@ecs.soton.ac.uk>
2012-02-24 12:50:27 +0000

    *Modified RunwayWithObstacle.java with new getters, setters and calculation methods

--- src/Model/trunk/SEG/src/model/RunwayWithObstacle.java
+++ src/Model/trunk/SEG/src/model/RunwayWithObstacle.java
@@ -5,29 +5,38 @@ public class RunwayWithObstacle {
 	private Runway runway;
 	private Obstacle obstacle;
 	private double distanceAwayFromThreshold; // In meter
+	private double angleOfSlope;
 
 	public RunwayWithObstacle(Runway runway, Obstacle obstacle,
 			double distanceAwayFromThreshold) {
-		super();
 		this.runway = runway;
 		this.obstacle = obstacle;
 		this.distanceAwayFromThreshold = distanceAwayFromThreshold;
+		this.angleOfSlope = 50;
+	}
+
+	public RunwayWithObstacle(Runway runway, Obstacle obstacle,
+			double distanceAwayFromThreshold, double angleOfSlope) {
+		this.runway = runway;
+		this.obstacle = obstacle;
+		this.distanceAwayFromThreshold = distanceAwayFromThreshold;
+		this.angleOfSlope = angleOfSlope;
 	}
 
 	public Runway getRunway() {
 		return runway;
 	}
 
-	public void setRunway(Runway runway) {
-		this.runway = runway;
-	}
-
 	public Obstacle getObstacle() {
 		return obstacle;
 	}
 
-	public void setObstacle(Obstacle obstacle) {
-		this.obstacle = obstacle;
+	public double getAngleOfSlope() {
+		return angleOfSlope;
+	}
+
+	public void setAngleOfSlope(double angleOfSlope) {
+		this.angleOfSlope = angleOfSlope;
 	}
 
 	public double getDistanceAwayFromThreshold() {
@@ -36,6 +45,41 @@ public class RunwayWithObstacle {
 
 	public void setDistanceAwayFromThreshold(double distanceAwayFromThreshold) {
 		this.distanceAwayFromThreshold = distanceAwayFromThreshold;
+	}
+	
+	public double getNewTORA(){
+		return 0;
+	}
+	
+	public double getNewASDA(){
+		return 0;
+	}
+	
+	public double getNewTODA(){
+		return 0;
+	}
+	
+	public double getNewLDA(){
+		return 0;
+	}
+
+	public void removeObstacle() {
+		obstacle = null;
+		distanceAwayFromThreshold = 0;
+		angleOfSlope = 50;
+	}
+
+	public void placeNewObstacle(Obstacle obstacle,
+			double distanceAwayFromThreshold) {
+		this.obstacle = obstacle;
+		this.distanceAwayFromThreshold = distanceAwayFromThreshold;
+	}
+
+	public void placeNewObstacle(Obstacle obstacle,
+			double distanceAwayFromThreshold, double angleOfSlope) {
+		this.obstacle = obstacle;
+		this.distanceAwayFromThreshold = distanceAwayFromThreshold;
+		this.angleOfSlope = angleOfSlope;
 	}
 
 }
