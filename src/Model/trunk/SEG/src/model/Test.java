2435de00f8f36864e345144bfeceaf830b9839f5
onme1g10 <onme1g10@ecs.soton.ac.uk>
2012-02-24 12:27:10 +0000

    solving a problem with an update

--- src/Model/trunk/SEG/src/model/Test.java
+++ src/Model/trunk/SEG/src/model/Test.java
@@ -6,33 +6,53 @@ public class Test {
 	
 	public static void main(String[] args) {
 		
-		Airport a = new Airport("JFK");
+		//create airport and 4 runways
+		Airport a = new Airport("Gatwick");
 		Runway r = new Runway("one", 888, 999, 777, 666);
 		Runway r1 = new Runway("two", 88, 99, 77, 66);
 		Runway r2 = new Runway("three", 8884, 9994, 7774, 6664);
 		Runway r3 = new Runway("four", 88568, 95699, 77567, 65666);
 		
+		//add runways to airport
 		a.addRunway(r);
 		a.addRunway(r1);
 		a.addRunway(r2);
 		a.addRunway(r3);
 		
+		//iterate over the runways in the airport and print all values
+		System.out.println("\n******************\nThis is the airport and its runways:\n" + a.getName());
 		for (Object o : a.runways()) {
 			System.out.println(((Runway) o).getName() +" "+ ((Runway) o).getASDA() +" "+ ((Runway) o).getLDA() +" "+ ((Runway) o).getTODA() +" "+ ((Runway) o).getTORA());
 		}
 		
+		//remove runway with name "one"
 		a.removeRunway("one");
 		
+		System.out.println("\nThis is the same airport with runway one removed:\n" + a.getName());
+		//iterate over the runways in the airport and print all values
 		for (Object o : a.runways()) {
 			System.out.println(((Runway) o).getName() +" "+ ((Runway) o).getASDA() +" "+ ((Runway) o).getLDA() +" "+ ((Runway) o).getTODA() +" "+ ((Runway) o).getTORA());
 		}
 		
+		//save airport a to xml file
 		a.saveToXML();
 		
-		/*
-		XMLFile x = new XMLFile();
-		System.out.println(x.loadAirport());
-		*/
+		//create a second airport and a loadxmlfile object
+		//load a file
+		Airport a2 = null;
+		LoadXMLFile lf = new LoadXMLFile();
+		try {
+			a2 = lf.loadFile();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("\nThis is the loaded airport:\n" + a.getName());
+		//iterate over the runways in the loaded airport and print all values
+		for (Object o : a2.runways()) {
+			System.out.println(((Runway) o).getName() +" "+ ((Runway) o).getASDA() +" "+ ((Runway) o).getLDA() +" "+ ((Runway) o).getTODA() +" "+ ((Runway) o).getTORA());
+		}
+		
 	}
 
 }
