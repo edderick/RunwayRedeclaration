7c1fcce538002b2a50216f7fad6b128188aa3424
ke1g10 <ke1g10@ecs.soton.ac.uk>
2012-02-27 17:44:54 +0000

    *Initial View coding, displays the topview runway. *Displays the runway and the tags. Resizes nicely. *Initial obstacle code written. *Used Edwards viewTest3 class with a few changes to set variables.

---
+++ src/View/MainFrame.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+
+public class MainFrame extends JFrame{
+
+	public static void main(String[] args){
+		MainFrame mf = new MainFrame();
+		
+		
+		JSplitPane b = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		JSplitPane a = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+		
+		
+		mf.getContentPane().add(a);
+		a.setRightComponent(b);
+		a.setLeftComponent(new JPanel());	
+		
+		TopView tv = new TopView();
+		tv.setRunwayDimensions(100, 20, "08L", "27R");
+		tv.setValues(0, 0, 0, 0, true, 50, 8, 200, 200);
+	
+		b.setTopComponent(tv);
+		b.setBottomComponent(new JPanel());
+		
+		//hackedy hack :D
+		mf.setVisible(true);
+		
+		a.setDividerLocation(0.5);
+		b.setDividerLocation(0.5);
+	
+	}
+	
+	public MainFrame(){
+		super("Main Frame");
+		
+		setSize(400,400);
+
+		setExtendedState(getExtendedState()|JFrame.MAXIMIZED_BOTH);
+
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	
+
+}
---
+++ src/View/TopView.java
@@ -0,0 +1,118 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+
+public class TopView extends JPanel {
+	
+	
+	//this value determines the amount of pixels the tag is from the end of the runway
+	final int tagBorder = 10;
+	
+	//this value determines how much of the width of the panel the runway takes up.
+	final double ratio = 0.2;
+	
+	
+	int TORA;
+	int TODA;
+	int ASDA;
+	int LDA;
+	int runwayWidth;
+	int runwayHeight;
+	int obstacleLength;
+	int obstacleWidth;
+	String leftTag;
+	String rightTag;
+	boolean obstacle;
+	
+	//relative to panel
+	int xRunway;
+	int yRunway;
+	
+	//relative to runway
+	int xObstacle;
+	int yObstacle;
+	
+	
+	
+		
+	public TopView(){
+		super();
+		setSize(300,200);
+		this.setBackground(Color.WHITE);
+		setVisible(true);
+			
+	}
+	
+	public void paint (Graphics g){
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D)g;
+		runwayCreation(g2d);
+		if(obstacle)obstacleCreation(g2d);
+			
+	}
+	
+	public void runwayCreation(Graphics2D g2d){
+		g2d.setColor(Color.GRAY);
+		
+		int pWidth = this.getWidth();
+		int pHeight = this.getHeight();
+		
+		
+		int width = (int) (ratio*pWidth);
+		int height = (width * runwayHeight)/runwayWidth;
+		
+			
+		//calculates the x and y values to position the runway on the view
+		xRunway = (int) (((1.0-ratio)/2) *pWidth);
+		yRunway = (pHeight - height)/2;
+		
+		g2d.fillRect(xRunway, yRunway, width, height);
+		
+		//27R 08L tags
+		g2d.setColor(Color.BLACK);
+		g2d.setFont(new Font("tag", 1, (int) (ratio*(pWidth*15)/225)));
+		g2d.rotate(0.5 * Math.PI,  xRunway+tagBorder, yRunway+(height/4));
+		g2d.drawString(leftTag, xRunway+ tagBorder, yRunway+(height/4));
+		g2d.rotate(-0.5 * Math.PI,  xRunway + tagBorder, yRunway+(height/4));
+		g2d.rotate(-0.5 * Math.PI,  xRunway + width - tagBorder, yRunway+(3*(height/4)));
+		g2d.drawString(rightTag, xRunway+ width - tagBorder, yRunway+(3*(height/4)));
+		
+		
+		
+	}
+	
+	public void obstacleCreation(Graphics2D g2d){
+		g2d.setColor(Color.RED);
+		int pWidth = this.getWidth();
+			
+		g2d.fillRect(100, 100, obstacleLength, obstacleWidth);
+	}
+	
+	public void setRunwayDimensions(int width, int height, String left, String right){
+		this.runwayWidth = width;
+		this.runwayHeight = height;
+		this.leftTag=left;
+		this.rightTag=right;
+	}
+	
+	public void setValues(int tora, int toda, int asda, int lda, boolean obstacle, int x, int y, int length, int width){
+		this.TORA = tora;
+		this.TODA = toda;
+		this.ASDA = asda;
+		this.LDA = lda;
+		this.obstacle = obstacle;
+		this.xObstacle = x;
+		this.yObstacle = y;
+		this.obstacleLength = length;
+		this.obstacleWidth = width;
+		
+		
+	}
+}
+
+
+
