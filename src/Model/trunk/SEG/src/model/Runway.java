69c07a4d727049476e96b1ce7e16125850da8f91
onme1g10 <onme1g10@ecs.soton.ac.uk>
2012-03-01 10:40:24 +0000

    Created array of two spaces for each parameter of the runway instead of one int, now index 0 is the original value and intdex 1 is the recalculated one.

--- src/Model/trunk/SEG/src/model/Runway.java
+++ src/Model/trunk/SEG/src/model/Runway.java
@@ -3,16 +3,26 @@ package model;
 public class Runway {
 
 	private String name;
-	private int TORA, ASDA, TODA, LDA, displacedThreshold;
+	private int[] TORA, ASDA, TODA, LDA, displacedThreshold;
 	
 	//Constructor
 	public Runway(String n, int tora, int asda, int toda, int lda, int disThres){
 		name = n;
-		TORA = tora;
-		ASDA = asda;
-		TODA = toda;
-		LDA = lda;
-		displacedThreshold = disThres;
+		TORA = new int[2];//tora;
+		TORA[0] = tora;
+		TORA[1] = tora;
+		ASDA = new int[2];//asda;
+		ASDA[0] = asda;
+		ASDA[1] = asda;
+		TODA = new int[2];//toda;
+		TODA[0] = toda;
+		TODA[1] = toda;
+		LDA = new int[2];//lda;
+		LDA[0] = lda;
+		LDA[1] = lda;
+		displacedThreshold = new int[2];//disThres;
+		displacedThreshold[0] = disThres;
+		displacedThreshold[1] = disThres;
 	}
 	
 	//Getters and Setters
@@ -24,44 +34,44 @@ public class Runway {
 		this.name = name;
 	}
 
-	public int getTORA() {
-		return TORA;
+	public int getTORA(int option) {
+		return TORA[option];
 	}
 
-	public void setTORA(int tORA) {
-		TORA = tORA;
+	public void setTORA(int option, int tORA) {
+		TORA[option] = tORA;
 	}
 
-	public int getASDA() {
-		return ASDA;
+	public int getASDA(int option) {
+		return ASDA[option];
 	}
 
-	public void setASDA(int aSDA) {
-		ASDA = aSDA;
+	public void setASDA(int option, int aSDA) {
+		ASDA[option] = aSDA;
 	}
 
-	public int getTODA() {
-		return TODA;
+	public int getTODA(int option) {
+		return TODA[option];
 	}
 
-	public void setTODA(int tODA) {
-		TODA = tODA;
+	public void setTODA(int option, int tODA) {
+		TODA[option] = tODA;
 	}
 
-	public int getLDA() {
-		return LDA;
+	public int getLDA(int option) {
+		return LDA[option];
 	}
 
-	public void setLDA(int lDA) {
-		LDA = lDA;
+	public void setLDA(int option, int lDA) {
+		LDA[option] = lDA;
 	}
 
-	public int getDisplacedThreshold() {
-		return displacedThreshold;
+	public int getDisplacedThreshold(int option) {
+		return displacedThreshold[option];
 	}
 
-	public void setDisplacedThreshold(int displacedThreshold) {
-		this.displacedThreshold = displacedThreshold;
+	public void setDisplacedThreshold(int option, int displacedThreshold) {
+		this.displacedThreshold[option] = displacedThreshold;
 	}
 
 	
