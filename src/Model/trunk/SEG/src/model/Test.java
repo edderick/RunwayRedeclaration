e0e66451927a71fd00d9fa91ebef7b6fd9212568
onme1g10 <onme1g10@ecs.soton.ac.uk>
2012-03-08 21:12:05 +0000

    Updated test file according to new changes

--- src/Model/trunk/SEG/src/model/Test.java
+++ src/Model/trunk/SEG/src/model/Test.java
@@ -7,31 +7,54 @@ public class Test {
 	public static void main(String[] args) {
 		
 		//create airport and 4 runways
-		Airport a = new Airport("Gatwick");
-		Runway r = new Runway("one", 888, 999, 777, 666, 0);
-		Runway r1 = new Runway("two", 88, 99, 77, 66, 306);
-		Runway r2 = new Runway("three", 8884, 9994, 7774, 6664, 0);
-		Runway r3 = new Runway("four", 88568, 95699, 77567, 65666, 307);
+		Airport a = new Airport("Heathrow");
+		Runway r = new Runway("27R", 888, 999, 777, 666, 0);
+		Runway r1 = new Runway("09L", 88, 99, 77, 66, 306);
+		Runway r2 = new Runway("27L", 8884, 9994, 7774, 6664, 0);
+		Runway r3 = new Runway("09R", 88568, 95699, 77567, 65666, 307);
 		
-		//add runways to airport
-		a.addRunway(r);
-		a.addRunway(r1);
-		a.addRunway(r2);
-		a.addRunway(r3);
+		PhysicalRunway one = new PhysicalRunway("27R/09L", r, r1);
+		PhysicalRunway two = new PhysicalRunway("27L/09R", r2, r3);
+		
+		//add physical runways to airport
+		a.addPhysicalRunway(one);
+		a.addPhysicalRunway(two);
+		
 		
 		//iterate over the runways in the airport and print all values
 		System.out.println("\n******************\nThis is the airport and its runways:\n" + a.getName());
 		for (Object o : a.runways()) {
-			System.out.println(((Runway) o).getName() +" "+ ((Runway) o).getASDA() +" "+ ((Runway) o).getLDA() +" "+ ((Runway) o).getTODA() +" "+ ((Runway) o).getTORA());
+			System.out.println(((PhysicalRunway) o).getId() +" "+ ((PhysicalRunway) o).getRunway(0).getASDA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(0).getName()
+					+" "+ ((PhysicalRunway) o).getRunway(0).getTORA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(0).getTODA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(0).getLDA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(0).getASDA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(1).getName()
+					+" "+ ((PhysicalRunway) o).getRunway(1).getTORA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(1).getTODA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(1).getLDA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(1).getASDA(1)
+					);
 		}
 		
 		//remove runway with name "one"
-		a.removeRunway("one");
+		a.removePhysicalRunway("one");
 		
 		System.out.println("\nThis is the same airport with runway one removed:\n" + a.getName());
 		//iterate over the runways in the airport and print all values
 		for (Object o : a.runways()) {
-			System.out.println(((Runway) o).getName() +" "+ ((Runway) o).getASDA() +" "+ ((Runway) o).getLDA() +" "+ ((Runway) o).getTODA() +" "+ ((Runway) o).getTORA());
+			System.out.println(((PhysicalRunway) o).getId() +" "+ ((PhysicalRunway) o).getRunway(0).getASDA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(0).getName()
+					+" "+ ((PhysicalRunway) o).getRunway(0).getTORA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(0).getTODA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(0).getLDA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(0).getASDA(1)
+					//+" "+ ((PhysicalRunway) o).getRunway(1).getTORA(1)
+					//+" "+ ((PhysicalRunway) o).getRunway(1).getTODA(1)
+					//+" "+ ((PhysicalRunway) o).getRunway(1).getLDA(1)
+					//+" "+ ((PhysicalRunway) o).getRunway(1).getASDA(1)
+					);
 		}
 		
 		//save airport a to xml file
@@ -40,17 +63,27 @@ public class Test {
 		//create a second airport and a loadxmlfile object
 		//load a file
 		Airport a2 = null;
-		LoadXMLFile lf = new LoadXMLFile();
+		/*LoadXMLFile lf = new LoadXMLFile();
 		try {
 			a2 = lf.loadFile();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
+		}*/
 		
 		System.out.println("\nThis is the loaded airport:\n" + a.getName());
 		//iterate over the runways in the loaded airport and print all values
-		for (Object o : a2.runways()) {
-			System.out.println(((Runway) o).getName() +" "+ ((Runway) o).getASDA() +" "+ ((Runway) o).getLDA() +" "+ ((Runway) o).getTODA() +" "+ ((Runway) o).getTORA());
+		for (Object o : a.runways()) {
+			System.out.println(((PhysicalRunway) o).getId() +" "+ ((PhysicalRunway) o).getRunway(0).getASDA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(0).getName()
+					+" "+ ((PhysicalRunway) o).getRunway(0).getTORA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(0).getTODA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(0).getLDA(1)
+					+" "+ ((PhysicalRunway) o).getRunway(0).getASDA(1)
+					//+" "+ ((PhysicalRunway) o).getRunway(1).getTORA(1)
+					//+" "+ ((PhysicalRunway) o).getRunway(1).getTODA(1)
+					//+" "+ ((PhysicalRunway) o).getRunway(1).getLDA(1)
+					//+" "+ ((PhysicalRunway) o).getRunway(1).getASDA(1)
+					);
 		}
 		
 	}
