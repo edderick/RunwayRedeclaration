8442cf2dbff0b5e81c6d30a227e912bad99f729a
ejfs1g10 <ejfs1g10@ecs.soton.ac.uk>
2012-04-22 20:01:23 +0000

    *Javadoc'd and slightly altered Airport

--- src/SEG_Airport/src/Model/Airport.java
+++ src/SEG_Airport/src/Model/Airport.java
@@ -4,57 +4,77 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 
-//Airport
+/**
+ * Represents an airport, contains runways
+ * @author Oscar
+ *
+ */
 public class Airport {
-	
+
 	//List of physical runways
 	private ArrayList<PhysicalRunway> runways;
 	private String name;
-	
-	//Constructor
-	public Airport(String n){
+
+	/**
+	 * The default constructor for Airport class
+	 * @param name The name of the airport
+	 */
+	public Airport(String name){
 		runways = new ArrayList<PhysicalRunway>();
-		name = n;
+		this.name = name;
 	}
-	
+
+	/**
+	 * @return The name of the airport 
+	 */
 	public String getName(){
 		return name;
 	}
-	
+
+	/**
+	 * @param name The new name for the airport
+	 */
 	public void setName(String name){
 		this.name = name;
 	}
-	
-	//Method to add runway to the list
-	public void addPhysicalRunway(PhysicalRunway r){
-		runways.add(r);
-	}
-	
-	//Method to remove runway from the list
-	public void removePhysicalRunway(String id){
-		
-		for (Iterator i = runways.iterator(); i.hasNext(); ) {
-		    Object element = i.next();
 
-		    if (((PhysicalRunway) element).getId() == id) {
-		        i.remove();
-		    }
+	/**
+	 * Adds a runway to the list
+	 * @param runway The Physical Runway to add to the list
+	 */
+	public void addPhysicalRunway(PhysicalRunway runway){
+		runways.add(runway);
+	}
+
+	/**
+	 * Removes a runway from  the list 
+	 * @param id The id of the runway to remove
+	 */
+	public void removePhysicalRunway(String id){
+		for(PhysicalRunway r : runways){
+			if(r.getId().equals(id)){
+				runways.remove(r);
+			}
 		}
 	}
-	
-	//Method that returns the whole list of runways
+
+	/**
+	 * @return List of runways
+	 */
 	public ArrayList<PhysicalRunway> runways(){
 		return runways;
 	}
-	
+
+	/**
+	 * Saves the airport to an xml file on disk
+	 */
 	public void saveToXML(){
-		
 		try {
 			SaveToXMLFile f = new SaveToXMLFile(this);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
