e67756d8c229b7985312d7c875fe00d3cb6c6573
onme1g10 <onme1g10@ecs.soton.ac.uk>
2012-05-02 00:48:19 +0000

    Added more test cases

--- src/SEG_Airport/src/Model/Tests_Oscar.java
+++ src/SEG_Airport/src/Model/Tests_Oscar.java
@@ -163,10 +163,59 @@ public class Tests_Oscar {
 			e.printStackTrace();
 		}
 		
-		
 		/*
 		 * ********
 		 * Test 4. 
+		 * ******** 
+		 * Step 1: Load an Airport from an XML file without prompting a dialog box.
+		 * Step 2: Print to stdout the values of the loaded Airport 
+		 * 			and compare them with the original ones.
+		 * Expected output: same values as in Airport airport1.
+		 */
+		
+		System.out.println("************\nTEST 4\n************");
+		
+		//Step 1.
+		LoadXMLFile lof3 = new LoadXMLFile();
+		Airport airport3 = lof3.silentLoadAirport("/Users/oscarmariani/Desktop/Airport.xml");
+		
+		// Step 2.
+		System.out.println("\nThis is the loaded airport:\n" + airport3.getName());
+		//iterate over the runways and physical runways in the loaded airport and print all values
+		
+		for (Object o : airport3.getPhysicalRunways()) {
+			System.out.println(((PhysicalRunway) o).getId()
+					+"\n resa: "+ ((PhysicalRunway) o).getRESA()
+					+"\n stopway: "+ ((PhysicalRunway) o).getStopway()
+					+"\n blast: "+ ((PhysicalRunway) o).getBlastAllowance()
+					+"\n stripWidth: "+ ((PhysicalRunway) o).getRunwayStripWidth()
+					+"\n clearedWidth: "+ ((PhysicalRunway) o).getClearedAndGradedWidth()
+					+"\n distanceFromThreshold: "+ ((PhysicalRunway) o).getDistanceAwayFromThreshold()
+					+"\n distanceFromCenterline: "+ ((PhysicalRunway) o).getDistanceAwayFromCenterLine()
+					+"\n angleOfSlope: "+ ((PhysicalRunway) o).getAngleOfSlope()
+					
+					
+					+"\n runway0 name: "+ ((PhysicalRunway) o).getRunway(0).getName() 
+					+"\n runway0 tora: "+ ((PhysicalRunway) o).getRunway(0).getTORA(1)
+					+"\n runway0 asda: "+ ((PhysicalRunway) o).getRunway(0).getASDA(1)
+					+"\n runway0 toda: "+ ((PhysicalRunway) o).getRunway(0).getTODA(1)
+					+"\n runway0 lda: "+ ((PhysicalRunway) o).getRunway(0).getLDA(1)
+					+"\n runway0 dt: "+ ((PhysicalRunway) o).getRunway(0).getDisplacedThreshold(Runway.DEFAULT)
+	
+					+"\n runway1 name: "+ ((PhysicalRunway) o).getRunway(1).getName()
+					+"\n runway1 tora: "+ ((PhysicalRunway) o).getRunway(1).getTORA(1)
+					+"\n runway1 asda: "+ ((PhysicalRunway) o).getRunway(1).getASDA(1)
+					+"\n runway1 toda: "+ ((PhysicalRunway) o).getRunway(1).getTODA(1)
+					+"\n runway1 lda: "+ ((PhysicalRunway) o).getRunway(1).getLDA(1)
+					+"\n runway1 dt: "+ ((PhysicalRunway) o).getRunway(0).getDisplacedThreshold(Runway.DEFAULT)
+					
+					);
+		}		
+		
+		
+		/*
+		 * ********
+		 * Test 5. 
 		 * ******** 
 		 * Step 1: Instantiate an Obstacle.
 		 * Step 2: Set its width and length values to random values.
@@ -174,7 +223,7 @@ public class Tests_Oscar {
 		 * Expected output: Same values as entered.
 		 */
 		
-		System.out.println("************\nTEST 4\n************");
+		System.out.println("************\nTEST 5\n************");
 		
 		// Step 1.
 		Obstacle obstacle = new Obstacle("boeing 747", 56.0);
@@ -190,13 +239,13 @@ public class Tests_Oscar {
 		
 		/*
 		 * ********
-		 * Test 5. 
+		 * Test 6. 
 		 * ******** 
 		 * Step 1: Save previous Obstacle to XML.
 		 * Step 2: Open the file saved and compare values with the expected ones.
 		 */
 		
-		System.out.println("************\nTEST 5\n************");
+		System.out.println("************\nTEST 6\n************");
 		
 		// Step 1.
 		obstacle.saveToXML();
@@ -204,7 +253,7 @@ public class Tests_Oscar {
 		
 		/*
 		 * ********
-		 * Test 6. 
+		 * Test 7. 
 		 * ******** 
 		 * Step 1: Load an Obstacle from an XML file.
 		 * Step 2: Print to stdout the values of the loaded Obstacle 
@@ -212,7 +261,7 @@ public class Tests_Oscar {
 		 * Expected output: same values as in Obstacle obstacle.
 		 */
 		
-		System.out.println("************\nTEST 6\n************");
+		System.out.println("************\nTEST 7\n************");
 		
 		//Step 1.
 		LoadXMLFile lof = new LoadXMLFile();
@@ -231,7 +280,7 @@ public class Tests_Oscar {
 		
 		/*
 		 * ********
-		 * Test 7. 
+		 * Test 8. 
 		 * ******** 
 		 * Step 1: Load an Obstacle from an XML file without prompting a dialog box.
 		 * Step 2: Print to stdout the values of the loaded Obstacle 
@@ -239,7 +288,7 @@ public class Tests_Oscar {
 		 * Expected output: same values as in Obstacle obstacle.
 		 */
 		
-		System.out.println("************\nTEST 7\n************");
+		System.out.println("************\nTEST 8\n************");
 		
 		//Step 1.
 		LoadXMLFile lof1 = new LoadXMLFile();
@@ -254,7 +303,7 @@ public class Tests_Oscar {
 		
 		/*
 		 * ********
-		 * Test 8. 
+		 * Test 9. 
 		 * ******** 
 		 * Step 1: Instantiate two Contacts.
 		 * Step 2: Create an ArrayList<Contact> and add both contacts to it
@@ -262,7 +311,7 @@ public class Tests_Oscar {
 		 * Expected output: Same values as entered.
 		 */
 		
-		System.out.println("************\nTEST 8\n************");
+		System.out.println("************\nTEST 9\n************");
 		
 		// Step 1.
 		Contact contact1 = new Contact("Oscar", "Mariani", "mariani.oscar@gamil.com");
@@ -277,6 +326,59 @@ public class Tests_Oscar {
 		for (Contact c : contactsList){
 			System.out.println("Contact: " + c.getFirstName() + " " + c.getLastName() + " - " + c.getEmail());
 		}
+		
+		
+		
+		/*
+		 * ********
+		 * Test 10. 
+		 * ******** 
+		 * Step 1: Save previous list of Contacts to XML.
+		 * Step 2: Open the file saved and compare values with the expected ones.
+		 */
+		
+		System.out.println("************\nTEST 10\n************");
+		
+		// Step 1.
+		try {
+			SaveToXMLFile saveTo = new SaveToXMLFile(contactsList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		/*
+		 * ********
+		 * Test 11. 
+		 * ******** 
+		 * Step 1: Load a list of Contacts from an XML file.
+		 * Step 2: Print to stdout the values of the loaded Contacts 
+		 * 			and compare them with the original ones.
+		 * Expected output: same values as in ArrayList<Contact> contactsList.
+		 */
+		
+		System.out.println("************\nTEST 11\n************");
+		
+		//Step 1.
+		ArrayList<Contact> contactsList2 = null;
+		LoadXMLFile lof2 = new LoadXMLFile();
+		try {
+			contactsList2 = lof2.loadContacts();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Step 2.
+		for (Contact c : contactsList2){
+			System.out.println("Contact: " + c.getFirstName() + " " + c.getLastName() + " - " + c.getEmail());
+		}
+		
+		
+		
+		
+		
+		
 		
 		
 		
